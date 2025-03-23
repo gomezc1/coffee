da6170d9d6666021f9c7bbf10896c91dfdde0a49
gomezc1 <75422709+gomezc1@users.noreply.github.com>
2025-03-23 01:24:48 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-
+<script>
+    document.querySelector('.btn').addEventListener('click', function() {
+        let name = prompt('What is your first name?');
+        if (name.length) {
+            alert('Thanks, ' + name + ' , Kindly wait to be called before placing your order c:');
+        } else {
+            alert('No name, no coffee for you :c');
+        }
+    });
+</script>
